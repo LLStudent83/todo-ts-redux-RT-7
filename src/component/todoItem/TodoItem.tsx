caa2dae606a0b01b2cc6260556e36c0817a918af
Dmitriy Prigulnov <prigulnov@mail.ru>
2023-12-05 21:08:59 +0500

    следующий коммит после отката

--- src/component/todoItem/TodoItem.tsx
+++ src/component/todoItem/TodoItem.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 function TodoItem({ todo, index }: Props): JSX.Element {
   console.log("log");
-
+  // следующий коммит после отката
   const dispatch = useAppDispatch();
   console.log("mount TodoItem");
   const classes = ["todoItem__todoTitle"];
